f1304cefec871e4d82c73c8503e649d05ce39e50
evingerbono <bono0823@gmail.com>
2023-03-17 13:41:51 +0100

    kép_megfordítása_és_visszafordítása

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
-const LIST = ['kepek/kep1.jpg', 'kepek/kep2.jpg', 'kepek/kep3.jpg', 'kepek/kep4.jpg', 'kepek/kep5.jpg', 'kepek/kep6.jpg','kepek/kep1.jpg', 'kepek/kep2.jpg', 'kepek/kep3.jpg', 'kepek/kep4.jpg', 'kepek/kep5.jpg', 'kepek/kep6.jpg']
-const db=0;
+const LIST = ['kepek/kep1.jpg', 'kepek/kep2.jpg', 'kepek/kep3.jpg', 'kepek/kep4.jpg', 'kepek/kep5.jpg', 'kepek/kep6.jpg', 'kepek/kep1.jpg', 'kepek/kep2.jpg', 'kepek/kep3.jpg', 'kepek/kep4.jpg', 'kepek/kep5.jpg', 'kepek/kep6.jpg']
+let db = 0;
+const KIVALASZTOTTKEPEK = []
 
 $(function () {
     const FESLO = $('#felso');
-    let tartalom=osszeAllit();
+    let tartalom = osszeAllit();
     FESLO.append(tartalom);
-    const FELSOKEPEK=$('#felso img');
-    FELSOKEPEK.on('click',kepreKattintas);
+    const FELSOKEPEK = $('#felso img');
+    FELSOKEPEK.on('click', kepreKattintas);
 })
 
 function osszeAllit() {
@@ -18,17 +19,28 @@ function osszeAllit() {
     return txt;
 }
 
-function kepreKattintas(){
-    const aktKep=event.target;
+function kepreKattintas() {
+    const aktKep = event.target;
     console.log($(aktKep).attr("id"));
-    aktKep.src=LIST[aktKep.id];
+    aktKep.src = LIST[aktKep.id];
+    KIVALASZTOTTKEPEK.push($(aktKep).attr("id"))
     db++;
-    if (db==2) {
-        db=0;
+    if (db == 2) {
+        db = 0;
         visszafordit();
     }
 }
 
-function visszafordit(){
-    
+function visszafordit() {
+
+    console.log(KIVALASZTOTTKEPEK);
+    setTimeout(function () {
+        const FELSOKEPEK = $('#felso img')
+        let aktKep = FELSOKEPEK.eq(KIVALASZTOTTKEPEK[0]);
+        $(aktKep).attr('src', 'kepek/hatter.jpg');
+        aktKep = FELSOKEPEK.eq(KIVALASZTOTTKEPEK[1]);
+        $(aktKep).attr('src', 'kepek/hatter.jpg');
+        KIVALASZTOTTKEPEK.pop();
+        KIVALASZTOTTKEPEK.pop();
+    }, 1000)
 }
