87e0b99cac34fe00c4ead900f917505fee8c5628
evingerbono <bono0823@gmail.com>
2023-03-17 13:16:57 +0100

    kep_megfordit

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 
-const LIST = ['kepek/kep1.jpg', 'kepek/kep2.jpg', 'kepek/kep3.jpg', 'kepek/kep4.jpg', 'kepek/kep5.jpg', 'kepek/kep6.jpg']
+const LIST = ['kepek/kep1.jpg', 'kepek/kep2.jpg', 'kepek/kep3.jpg', 'kepek/kep4.jpg', 'kepek/kep5.jpg', 'kepek/kep6.jpg','kepek/kep1.jpg', 'kepek/kep2.jpg', 'kepek/kep3.jpg', 'kepek/kep4.jpg', 'kepek/kep5.jpg', 'kepek/kep6.jpg']
+const db=0;
 
 $(function () {
     const FESLO = $('#felso');
     let tartalom=osszeAllit();
     FESLO.append(tartalom);
+    const FELSOKEPEK=$('#felso img');
+    FELSOKEPEK.on('click',kepreKattintas);
 })
 
 function osszeAllit() {
     let txt = "";
     for (let index = 0; index < LIST.length; index++) {
-        txt += `<div><img src="${LIST[index]}"></div>`;
+        txt += `<div><img src="kepek/hatter.jpg" alt="" id="${index}"></div>`;
     }
     return txt;
+}
+
+function kepreKattintas(){
+    const aktKep=event.target;
+    console.log($(aktKep).attr("id"));
+    aktKep.src=LIST[aktKep.id];
+    db++;
+    if (db==2) {
+        db=0;
+        visszafordit();
+    }
+}
+
+function visszafordit(){
+    
 }
