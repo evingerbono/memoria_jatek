4da3e840849649a7a75ddbd527220ef0355e4ce2
evingerbono <bono0823@gmail.com>
2023-03-17 12:38:30 +0100

    képek_berakása_HTMbe

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-$(function(){
-    console.log("Bármi");
+
+const LIST = ['kepek/kep1.jpg', 'kepek/kep2.jpg', 'kepek/kep3.jpg', 'kepek/kep4.jpg', 'kepek/kep5.jpg', 'kepek/kep6.jpg']
+
+$(function () {
+    const FESLO = $('#felso');
+    let tartalom=osszeAllit();
+    FESLO.append(tartalom);
 })
+
+function osszeAllit() {
+    let txt = "";
+    for (let index = 0; index < LIST.length; index++) {
+        txt += `<div><img src="${LIST[index]}"></div>`;
+    }
+    return txt;
+}
